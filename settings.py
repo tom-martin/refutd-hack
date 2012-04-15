7078d4624c0b05402d32969cce48fffe6187a8d2
Tom Martin <tompoges@googlemail.com>
2012-04-15 10:31:20 +0100

    Bootstrap (need to install django-bootstrap)

--- settings.py
+++ settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'refutd_hack',
+    'bootstrap',
 )
 
 # A sample logging configuration. The only tangible logging
